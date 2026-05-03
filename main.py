86259b0ce29a4d3ae7df96af773ce9b8201adc0b
Wang Wz <w724595267@outlook.com>
2026-05-03 16:38:50 +0800

    refactor: 移除未使用的统一消息来源字典，优化消息发送逻辑

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ class WuxiaNewsNotic(Star):
         super().__init__(context)
         self._task_event: asyncio.Event = asyncio.Event()
         self._task: asyncio.Task | None = None
-        self._unified_msg_origin: dict[str, str] = {}
         self.logger = logger
         self.config = cast(Config, config)
         self.conf_notic = Notic.model_validate(self.config.notic)
@@ -35,11 +34,6 @@ class WuxiaNewsNotic(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
         self.logger.info("天刀公告插件初始化")
         await init_news_cache()
-
-        self._unified_msg_origin = {
-            qq_group_id: f"{MessageSesion.platform_id}:GroupMessage:{qq_group_id}"
-            for qq_group_id in self.config.subscribe
-        }
 
         async def func():
             while True:
@@ -87,16 +81,15 @@ class WuxiaNewsNotic(Star):
                 from data.plugins.astrbot_plugin_nobrowser_markdown_to_pic.main import (
                     MyPlugin as Mk2Img,
                 )
+
                 if isinstance(mk2img_instanc_metadata.star_cls, Mk2Img):
                     async with ClientSession() as session:
                         await news.get_content(session)
                     mk2img_instanc = cast(Mk2Img, mk2img_instanc_metadata.star_cls)
-                    img = await pillowmd.MdToImage(news.content,useImageUrl=True)
+                    img = await pillowmd.MdToImage(news.content, useImageUrl=True)
                     img_path = await mk2img_instanc._save_temp_image(img)
-                    msg_chain = (
-                        MessageChain().message(news.url).file_image(img_path)
-                    )
-                
+                    msg_chain = MessageChain().message(news.url).file_image(img_path)
+
             case "url":
                 msg_chain = MessageChain().message(news.title).message(news.url)
 
@@ -105,11 +98,12 @@ class WuxiaNewsNotic(Star):
             return
 
         for qq_group_id in self.config.subscribe:
-            if umo := self._unified_msg_origin.get(qq_group_id):
-                await StarTools.send_message(umo, msg_chain)
-                self.logger.info(
-                    f"发送公告到群：{qq_group_id}, 公告：{news.title} - {news.time}"
-                )
+            await StarTools.send_message_by_id(
+                type="GroupMessage", id=qq_group_id, message_chain=msg_chain
+            )
+            self.logger.info(
+                f"发送公告到群：{qq_group_id}, 公告：{news.title} - {news.time}"
+            )
 
     @filter.command("公告")
     async def news(self, event: AstrMessageEvent):
@@ -125,13 +119,11 @@ class WuxiaNewsNotic(Star):
     async def set_subscription(self, event: AstrMessageEvent):
         umo = event.unified_msg_origin
         group_id = event.get_group_id()
-        if group_id in self._unified_msg_origin:
-            self._unified_msg_origin.pop(group_id)
+        if group_id in self.config.subscribe:
             self.config.subscribe.remove(group_id)
             self.logger.info(f"取消订阅uid：{group_id},{umo}")
             yield event.plain_result("取消订阅成功！")
         else:
-            self._unified_msg_origin[group_id] = umo
             self.config.subscribe.append(group_id)
             self.logger.info(f"添加订阅uid：{group_id},{umo}")
             yield event.plain_result("订阅成功！")
